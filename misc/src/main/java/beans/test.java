fd902f77635782635887769288c2f84552967aa1
liupandeng <345433326@qq.com>
2015-05-09 00:04:40 +0800

    tijiao

---
+++ misc/src/main/java/beans/test.java
@@ -0,0 +1,11 @@
+package beans;
+
+/**
+ * Created by Administrator on 2015/5/8.
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.print("hello");
+    }
+
+}
